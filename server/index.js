f10b0ca3e5a72c307e3cdcbddd8e33d8ba96f5d4
NTLUONG <ntluongbn62@gmail.com>
2022-04-25 22:30:44 +0800

    ngothiluong

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express =require('express')
+const path = require('path')
+
+const app = express()
+
+const buildDir = path.join(__dirname,'../build')
+const subDir ='/'
+const logRequests = false
+
+app.use(subDir, express.static(buildDir))
+
+app.get('*',(req,res)=>{
+    if(logRequests){
+        console.log(req.method + '' + req.url);
+    }
+    res.sendFile(path.join(buildDir,'index.html'))
+})
+
+app.listen(process.env.PORT || 3000)
