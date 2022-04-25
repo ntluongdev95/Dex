39878723461631e4e43fa37d16fa9a981a03254a
NTLUONG <ntluongbn62@gmail.com>
2022-04-25 22:46:52 +0800

    ntl

--- server/index.js
+++ server/index.js
@@ -1,20 +1,6 @@
 const express =require('express')
-const path = require('path')
 
 const app = express()
-
-const buildDir = path.join(__dirname,'../build')
-const subDir ='/'
-const logRequests = false
-
-app.use(subDir, express.static(buildDir))
-
-app.get('*',(req,res)=>{
-    if(logRequests){
-        console.log(req.method + '' + req.url);
-    }
-    res.sendFile(path.join(buildDir,'index.html'))
-})
 
 app.listen(process.env.PORT || 3000)
 console.log('react app is runing on the port 3000');
